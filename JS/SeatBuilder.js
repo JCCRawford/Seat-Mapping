90c92e6c77981a1e46ac05380a9a97a139cf5979
Jonathon Crawford <jonathonccrawford@gmail.com>
2020-01-30 02:13:32 -0500

    created git repository

---
+++ JS/SeatBuilder.js
@@ -0,0 +1,4 @@
+
+function buildSeatSVG(){
+    return "<svg width='50px' height='50px'> <rect x='0px' y ='0px' width='100%' height='100%' style='fill:white;stroke:black;stroke-width:3px;'></rect><rect x='0px' y ='0px' width='20%' height='75%' style='fill:white;stroke:black;stroke-width:3px;'></rect><rect x='80%' y ='0px' width='20%' height='75%' style='fill:white;stroke:black;stroke-width:3px;'></rect><rect x='0' y ='75%' width='100%' height='25%' style='fill:white;stroke:black;stroke-width:3px;'></rect></svg>";
+}
