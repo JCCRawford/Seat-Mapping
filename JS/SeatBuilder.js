efffff2542a588791eb5c1cfd150673ed44c6ec9
Jonathon Crawford <jonathonccrawford@gmail.com>
2020-01-30 21:20:15 -0500

    added margin around seat svg

--- JS/SeatBuilder.js
+++ JS/SeatBuilder.js
@@ -2,5 +2,5 @@
 function buildSeatSVG(){
     //return "<img src='SVGS/seat.svg'>"
     
-   return "<svg width='50px' height='50px'> <rect x='0px' y ='0px' width='100%' height='100%' style='fill:white;stroke:black;stroke-width:3px;'></rect><rect x='0px' y ='0px' width='20%' height='75%' style='fill:white;stroke:black;stroke-width:3px;'></rect><rect x='80%' y ='0px' width='20%' height='75%' style='fill:white;stroke:black;stroke-width:3px;'></rect><rect x='0' y ='75%' width='100%' height='25%' style='fill:white;stroke:black;stroke-width:3px;'></rect></svg>";
+   return "<svg width='50px' height='50px'><svg width='90%' height='90%' x='5%' y='5%'> <rect x='0px' y ='0px' width='100%' height='100%' style='fill:white;stroke:black;stroke-width:3px;'></rect><rect x='0px' y ='0px' width='20%' height='75%' style='fill:white;stroke:black;stroke-width:3px;'></rect><rect x='80%' y ='0px' width='20%' height='75%' style='fill:white;stroke:black;stroke-width:3px;'></rect><rect x='0' y ='75%' width='100%' height='25%' style='fill:white;stroke:black;stroke-width:3px;'></rect></svg></svg>";
 }
